e8898e8098504f3651cbf0357950fc9fe6ac1b7a
Kostroun Alex <stingwray.mail@gmail.com>
2021-11-20 22:49:30 +0100

    first version of sw treasure generator

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -75,124 +75,180 @@
     }
 
     app.swTreasureTradeouts = () => {
-        let resultTemplate = document.querySelector('.sw-gen-line.template');
+        let resultTemplate = document.querySelector('.sw-gen-container.template');
+        let resultItemTemplate = document.querySelector('.sw-gen-item.template');
         let resultContainer = document.querySelector('.sw-gen-results');
 
         let encounterXP = document.querySelector('.sw-input-value').value;
         let totalGold = (Math.floor(Math.random()*3) + 1)*encounterXP;
-
-        //$('.sw-gen-line.generated').remove();
+        let timestamp = new Date();
 
         let newResultLine = resultTemplate.cloneNode(true);
-        newResultLine.classList.add('generated', 'top-brd-dash');
-        newResultLine.classList.remove ('template');
-        newResultLine.innerHTML = '[' + new Date().toLocaleTimeString() + ']' + 'Total gold: ' + totalGold;
 
-        resultContainer.appendChild(newResultLine);
+        newResultLine.classList.add('generated');
+        newResultLine.classList.remove ('template');
+
+        let fnPrependNewItem = (text, weight = null) => {
+            let newResultItem = resultItemTemplate.cloneNode(true);
+            newResultItem.classList.add('generated');
+            newResultItem.classList.remove ('template');
+            newResultItem.querySelector('.item-description').innerHTML = text;
+            newResultItem.querySelector('.item-weight').innerHTML = weight;
+
+            newResultLine.prepend(newResultItem);
+        };
+
+        //$('.sw-gen-container.generated').remove();
 
         let calculation = new Promise ((resolve) => {
             let tradeouts5000 = Math.floor(totalGold/5000);
-            let resultsInit = [...Array(tradeouts5000).keys()].map(x => x = Math.floor(Math.random()*9 + 1)).filter(x => x === 1).length;
-            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.floor(Math.random()*19 + 1));
-            let resultsMajorMagicItems = resultsTypes.filter(x => x === 20);
-            let resultsMajorGems = resultsTypes.filter(x => x < 20).map(x => x = Math.floor);
+            let resultsInit = [...Array(tradeouts5000).keys()].map(x => x = Math.round(Math.random()*9) + 1).filter(x => x === 1).length;
+            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.round(Math.random()*19) + 1);
+            let resultsMajorGemsTypes = resultsTypes.filter(x => x < 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMajorGemsTypeOne = resultsMajorGemsTypes.filter(x => x === 1).map(x => x = (Math.round(Math.random()*99) + 1)*10);
+            let resultsMajorGemsTypeTwo = resultsMajorGemsTypes.filter(x => x === 2).map(x => x = (Math.round(Math.random()*99) + 1)*80);
+            let resultsMajorGemsTypeThree = resultsMajorGemsTypes.filter(x => x === 3).map(x => x = (Math.round(Math.random()*99) + 1)*120);
+            let resultsMajorGemsTypeFour = resultsMajorGemsTypes.filter(x => x === 4).map(x => x = (Math.round(Math.random()*99) + 1)*200);
+            let resultsMajorMagicItemTypes = resultsTypes.filter(x => x === 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMajorMagicItemTypeOne = resultsMajorMagicItemTypes.filter(x => x === 1).map(x => x = Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1);
+            let resultsMajorMagicItemTypeTwo = resultsMajorMagicItemTypes.filter(x => x === 2).map(x => x = Math.round(Math.random()*5) + 1 + 12);
+            let resultsMajorMagicItemTypeThree = resultsMajorMagicItemTypes.filter(x => x === 3).map(x => x = Math.round(Math.random()*5) + 1 + 12);
+            let resultsMajorMagicItemTypeFour = resultsMajorMagicItemTypes.filter(x => x === 4).map(x => x = Math.round(Math.random()*19) + 1 + 40);
+
+            console.log('Total gold pool for [' + timestamp.toLocaleTimeString() + ']: ' + totalGold);
 
             if (resultsInit > 0) {
-                let newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = '5000 Trade-Out #: ' + resultsInit;
+                totalGold -= resultsInit*5000;
+                console.log('5000 Trade-Out #: ' + resultsInit);
+                console.log('Gold after 5000 Trade Out: ' + totalGold);
 
-                resultContainer.appendChild(newResultLine);
-
-                if (resultsMajorMagicItems.length > 0) {
-                    newResultLine = resultTemplate.cloneNode(true);
-                    newResultLine.classList.add('generated');
-                    newResultLine.classList.remove ('template');
-                    newResultLine.innerHTML = 'Major Magic Items: ' + resultsMajorMagicItems;
-    
-                    resultContainer.appendChild(newResultLine);
+                if (resultsMajorMagicItemTypeOne.length > 0) {
+                    fnPrependNewItem('Potions (Table 85): ' + resultsMajorMagicItemTypeOne.join(', '));
+                };
+                if (resultsMajorMagicItemTypeTwo.length > 0) {
+                    fnPrependNewItem('Scrolls (Table 86): ' + resultsMajorMagicItemTypeTwo.join(', '));
+                };
+                if (resultsMajorMagicItemTypeThree.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 89): ' + resultsMajorMagicItemTypeThree.join(', '));
+                };
+                if (resultsMajorMagicItemTypeFour.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 98): ' + resultsMinorMagicItemTypeFour.join(', '));
+                };
+                if (resultsMajorGemsTypes.length > 0) {
+                    fnPrependNewItem('Major Gems & Jewelry: ' + resultsMajorGemsTypeOne.concat(resultsMajorGemsTypeTwo).concat(resultsMajorGemsTypeThree).concat(resultsMajorGemsTypeFour).join(', '));
                 };
                 
-                newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = 'Major Gems & Jewelry: ' + resultsMajorGems;
-
-                resultContainer.appendChild(newResultLine);
             };
-
             resolve();
         }).then(() => {
             let tradeouts1000 = Math.floor(totalGold/1000);
-            let resultsInit = [...Array(tradeouts1000).keys()].map(x => x = Math.floor(Math.random()*9 + 1)).filter(x => x === 1).length;
-            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.floor(Math.random()*19 + 1));
-            let resultsMediumMagicItems = resultsTypes.filter(x => x === 20);
-            let resultsMediumGems = resultsTypes.filter(x => x < 20);
+            let resultsInit = [...Array(tradeouts1000).keys()].map(x => x = Math.round(Math.random()*9) + 1).filter(x => x === 1).length;
+            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.round(Math.random()*19) + 1);
+            let resultsMediumGemsTypes = resultsTypes.filter(x => x < 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMediumGemsTypeOne = resultsMediumGemsTypes.filter(x => x === 1).map(x => x = Math.round(Math.random()*99) + 1);
+            let resultsMediumGemsTypeTwo = resultsMediumGemsTypes.filter(x => x === 2).map(x => x = (Math.round(Math.random()*5) + 1)*200);
+            let resultsMediumGemsTypeThree = resultsMediumGemsTypes.filter(x => x === 3).map(x => x = (Math.round(Math.random()*5) + 1)*300);
+            let resultsMediumGemsTypeFour = resultsMediumGemsTypes.filter(x => x === 4).map(x => x = (Math.round(Math.random()*99) + 1)*100);
+            let resultsMediumMagicItemTypes = resultsTypes.filter(x => x === 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMediumMagicItemTypeOne = resultsMediumMagicItemTypes.filter(x => x === 1).map(x => x = Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1 + ', ' + Math.round(Math.random()*99) + 1);
+            let resultsMediumMagicItemTypeTwo = resultsMediumMagicItemTypes.filter(x => x === 2).map(x => x = Math.round(Math.random()*5) + 1 + 6);
+            let resultsMediumMagicItemTypeThree = resultsMediumMagicItemTypes.filter(x => x === 3).map(x => x = Math.round(Math.random()*5) + 1 + 6);
+            let resultsMediumMagicItemTypeFour = resultsMediumMagicItemTypes.filter(x => x === 4).map(x => x = Math.round(Math.random()*19) + 1 + 20);
 
             if (resultsInit > 0) {
-                let newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = '1000 Trade-Out #: ' + resultsInit;
+                totalGold -= resultsInit*1000;
+                console.log('1000 Trade-Out #: ' + resultsInit);
+                console.log('Gold after 1000 Trade Out: ' + totalGold);
 
-                resultContainer.appendChild(newResultLine);
-                
-                if (resultsMediumMagicItems.length > 0) {
-                    newResultLine = resultTemplate.cloneNode(true);
-                    newResultLine.classList.add('generated');
-                    newResultLine.classList.remove ('template');
-                    newResultLine.innerHTML = 'Medium Magic Items: ' + resultsMediumMagicItems;
-    
-                    resultContainer.appendChild(newResultLine);
+                if (resultsMediumMagicItemTypeOne.length > 0) {
+                    fnPrependNewItem('Potions (Table 85): ' + resultsMediumMagicItemTypeOne.join(', '));
                 };
-            
-                newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = 'Medium Gems & Jewelry: ' + resultsMediumGems;
-
-                resultContainer.appendChild(newResultLine);
-            }
-            
+                if (resultsMediumMagicItemTypeTwo.length > 0) {
+                    fnPrependNewItem('Scrolls (Table 86): ' + resultsMediumMagicItemTypeTwo.join(', '));
+                };
+                if (resultsMediumMagicItemTypeThree.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 89): ' + resultsMediumMagicItemTypeThree.join(', '));
+                };
+                if (resultsMediumMagicItemTypeFour.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 98): ' + resultsMinorMagicItemTypeFour.join(', '));
+                };
+                if (resultsMediumGemsTypes.length > 0) {
+                    fnPrependNewItem('Medium Gems & Jewelry: ' + resultsMediumGemsTypeOne.concat(resultsMediumGemsTypeTwo).concat(resultsMediumGemsTypeThree).concat(resultsMediumGemsTypeFour).join(', '));
+                };
+            };
         }).then(() => {
             let tradeouts100 = Math.floor(totalGold/100);
-            let resultsInit = [...Array(tradeouts100).keys()].map(x => x = Math.floor(Math.random()*9 + 1)).filter(x => x === 1).length;
-            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.floor(Math.random()*19 + 1));
-            let resultsMinorMagicItems = resultsTypes.filter(x => x === 20);
-            let resultsMinorGemsTypes = resultsTypes.filter(x => x < 20).map(x => x = Math.floor(Math.random()*3 + 1));
-            let resultsMinorGemsTypeOne = resultsMinorGemsTypes.filter(x => x === 1).map(x => x = Math.floor(Math.random()*5 + 1));
-            let resultsMinorGemsTypeTwo = resultsMinorGemsTypes.filter(x => x === 2).map(x => x = Math.floor(Math.random()*99 + 1 + 25));
-            let resultsMinorGemsTypeThree = resultsMinorGemsTypes.filter(x => x === 2).map(x => x = Math.floor(Math.random()*99 + 1 + 75));
-            let resultsMinorGemsTypeFour = resultsMinorGemsTypes.filter(x => x === 2).map(x => x = Math.floor(Math.random()*99 + 1)*10);
+            let resultsInit = [...Array(tradeouts100).keys()].map(x => x = Math.round(Math.random()*9) + 1).filter(x => x === 1).length;
+            let resultsTypes = [...Array(resultsInit).keys()].map(x => x = Math.round(Math.random()*19) + 1);
+            let resultsMinorGemsTypes = resultsTypes.filter(x => x < 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMinorGemsTypeOne = resultsMinorGemsTypes.filter(x => x === 1).map(x => x = Math.round(Math.random()*5) + 1);
+            let resultsMinorGemsTypeTwo = resultsMinorGemsTypes.filter(x => x === 2).map(x => x = Math.round(Math.random()*99) + 1 + 25);
+            let resultsMinorGemsTypeThree = resultsMinorGemsTypes.filter(x => x === 3).map(x => x = Math.round(Math.random()*99) + 1 + 75);
+            let resultsMinorGemsTypeFour = resultsMinorGemsTypes.filter(x => x === 4).map(x => x = (Math.round(Math.random()*99) + 1)*10);
+            let resultsMinorMagicItemTypes = resultsTypes.filter(x => x === 20).map(x => x = Math.round(Math.random()*3) + 1);
+            let resultsMinorMagicItemTypeOne = resultsMinorMagicItemTypes.filter(x => x === 1).map(x => x = Math.round(Math.random()*99) + 1);
+            let resultsMinorMagicItemTypeTwo = resultsMinorMagicItemTypes.filter(x => x === 2).map(x => x = Math.round(Math.random()*5) + 1);
+            let resultsMinorMagicItemTypeThree = resultsMinorMagicItemTypes.filter(x => x === 3).map(x => x = Math.round(Math.random()*5) + 1);
+            let resultsMinorMagicItemTypeFour = resultsMinorMagicItemTypes.filter(x => x === 4).map(x => x = Math.round(Math.random()*19) + 1);
 
             if (resultsInit > 0) {
-                let newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = '100 Trade-Out #: ' + resultsInit;
+                totalGold -= resultsInit*100;
+                console.log('100 Trade-Out #: ' + resultsInit);
+                console.log('Gold after 100 Trade Out: ' + totalGold);
 
-                resultContainer.appendChild(newResultLine);
-
-                if (resultsMinorMagicItems.length > 0) {
-                    newResultLine = resultTemplate.cloneNode(true);
-                    newResultLine.classList.add('generated');
-                    newResultLine.classList.remove ('template');
-                    newResultLine.innerHTML = 'Minor Magic Items: ' + resultsMinorMagicItems;
-    
-                    resultContainer.appendChild(newResultLine);
+                if (resultsMinorMagicItemTypeOne.length > 0) {
+                    fnPrependNewItem('Potions (Table 85): ' + resultsMinorMagicItemTypeOne.join(', '));
                 };
-
-                newResultLine = resultTemplate.cloneNode(true);
-                newResultLine.classList.add('generated');
-                newResultLine.classList.remove ('template');
-                newResultLine.innerHTML = 'Minor Gems & Jewelry: ' + resultsMinorGemsTypeOne.concat(resultsMinorGemsTypeTwo).concat(resultsMinorGemsTypeThree).concat(resultsMinorGemsTypeFour).join(', ');
-
-                resultContainer.appendChild(newResultLine);
+                if (resultsMinorMagicItemTypeTwo.length > 0) {
+                    fnPrependNewItem('Scrolls (Table 86): ' + resultsMinorMagicItemTypeTwo.join(', '));
+                };
+                if (resultsMinorMagicItemTypeThree.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 89): ' + resultsMinorMagicItemTypeThree.join(', '));
+                };
+                if (resultsMinorMagicItemTypeFour.length > 0) {
+                    fnPrependNewItem('Magic Armor & Weapons (Table 98): ' + resultsMinorMagicItemTypeFour.join(', '));
+                };
+                if (resultsMinorGemsTypes.length > 0) {
+                    fnPrependNewItem('Minor Gems & Jewelry: ' + resultsMinorGemsTypeOne.concat(resultsMinorGemsTypeTwo).concat(resultsMinorGemsTypeThree).concat(resultsMinorGemsTypeFour).join(', '));
+                };
             };
+        }).then(() => {
+            let tradeoutAmount = Math.round(totalGold*Math.random());
+            let tradeoutNumber = Math.round(Math.random()*3) + 1;
+            let tradeoutsAntique = Math.floor(tradeoutAmount/tradeoutNumber);
+            let resultsTypes = [...Array(tradeoutNumber).keys()].map(x => x = Math.round(Math.random()*19) + 1);
+            let resultsAntiqueTypeOne = resultsTypes.filter(x => x <= 10).map(x => x = Math.round((Math.random() + 0.5)*tradeoutsAntique*1));
+            let resultsAntiqueTypeOneWeights = resultsTypes.filter(x => x <= 10).map(x => x = Math.round(Math.random()*9) + 1);
+            let resultsAntiqueTypeTwo = resultsTypes.filter(x => x > 10 && x <= 15).map(x => x = Math.round((Math.random() + 0.75)*tradeoutsAntique*1));
+            let resultsAntiqueTypeTwoWeights = resultsTypes.filter(x => x > 10 && x <= 15).map(x => x = Math.round(Math.random()*19) + 1);
+            let resultsAntiqueTypeThree = resultsTypes.filter(x => x > 15 && x <= 18).map(x => x = Math.round((Math.random() + 1)*tradeoutsAntique*1));
+            let resultsAntiqueTypeThreeWeights = resultsTypes.filter(x => x > 15 && x <= 18).map(x => x = Math.round(Math.random()*6) + 1);
+            let resultsAntiqueTypeFour = resultsTypes.filter(x => x > 18).map(x => x = Math.round((Math.random() + 2)*tradeoutsAntique*1));
+            let resultsAntiqueTypeFourWeights = resultsTypes.filter(x => x > 18).map(x => x = Math.round(Math.random()*6) + 1);
+
+            totalGold -= tradeoutAmount;
+            console.log('Antique Trade-Out #: ' + tradeoutNumber);
+            console.log('Gold after antique Trade Out: ' + (totalGold));
+
+            if (resultsAntiqueTypeOne.length > 0) {
+                fnPrependNewItem('Common Items: ' + resultsAntiqueTypeOne.join(', '), 'Weights, lbs: ' + resultsAntiqueTypeOneWeights.join(', '));
+            };
+            if (resultsAntiqueTypeTwo.length > 0) {
+                fnPrependNewItem('Depictions: ' + resultsAntiqueTypeTwo.join(', '), 'Weights, lbs: ' + resultsAntiqueTypeTwoWeights.join(', '));
+            };
+            if (resultsAntiqueTypeThree.length > 0) {
+                fnPrependNewItem('Writings: ' + resultsAntiqueTypeThree.join(', '), 'Weights, lbs: ' + resultsAntiqueTypeThreeWeights.join(', '));
+            };
+            if (resultsAntiqueTypeFour.length > 0) {
+                fnPrependNewItem('Artifacts: ' + resultsAntiqueTypeFour.join(', '), 'Weights, lbs: ' + resultsAntiqueTypeFourWeights.join(', '));
+            };
+            fnPrependNewItem('Gold coins: ' + totalGold);
         });
-        
-        
+
+        calculation.then(() => {
+            fnPrependNewItem('[' + timestamp.toLocaleTimeString() + ']');
+            resultContainer.prepend(newResultLine);
+        });        
     };
 
     $(function() {
